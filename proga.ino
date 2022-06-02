2d4b6d0e5244d37307c617a5dd14738e1bbd032b
katuha007 <106179734+katuha007@users.noreply.github.com>
2022-06-02 19:54:21 +0300

    Update proga.ino

--- proga.ino
+++ proga.ino
@@ -1,77 +1,142 @@
 #include <AFMotor.h>
 #include <Servo.h>
 int val;
-Servo serv;
-AF_DCMotor motor1(2); 
+int y1 = 0;
+int y2 = 0;
+int last = 0;
+Servo servl;
+Servo servp;
+AF_DCMotor motor1(2);
 AF_DCMotor motor2(4);
 void setup()
-{ serv.attach(9); 
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT); // СТРЕЛЬБА
+  pinMode(7,OUTPUT); // при перезарядке
+  pinMode(5,OUTPUT);// при движении
+  servl.attach(10);
+  servp.attach(9);
   Serial.begin(9600);
-  
-  
+  digitalWrite(12, 0);
+  digitalWrite(13 , 0);
+  digitalWrite(7, 0);
+  digitalWrite(5, 0);
+   digitalWrite(8, 0);
 }
 void loop()
 {
-  if (Serial.available())
+  if ( Serial.available() )
   {
     val = Serial.read() - '0' ;
     if (val == 1)
     {
-  motor1.run(BACKWARD);  
-  motor2.run(BACKWARD);  
-  motor1.setSpeed(255);   
-  motor2.setSpeed(255); 
+      motor1.run(BACKWARD);
+      motor2.run(BACKWARD);
+      motor1.setSpeed(255);
+      motor2.setSpeed(255);
+      digitalWrite(12, 0);
+      digitalWrite(13 , 0);
+     digitalWrite(7, 0);
+  digitalWrite(5 , 1);
+   digitalWrite(8, 0);
+      
     }
     if ( val == 0 )
     {
-  motor1.setSpeed(0);   
-  motor2.setSpeed(0); 
+      motor1.setSpeed(0);
+      motor2.setSpeed(0);
+      digitalWrite(12, 0);
+      digitalWrite(13 , 0);
+    digitalWrite(7, 0);
+  digitalWrite(5 , 0);
+   digitalWrite(8 , 0);
     }
-   if (val == 2) {
-  motor1.run(FORWARD);  
-  motor2.run(BACKWARD);  
-  motor1.setSpeed(200);   
-  motor2.setSpeed(200);
+    if (val == 2) {
+      motor1.run(FORWARD);
+      motor2.run(BACKWARD);
+      motor1.setSpeed(250);
+      motor2.setSpeed(250);
+      digitalWrite(12, 0);
+      digitalWrite(13 , 0);
+        digitalWrite(7, 0);
+  digitalWrite(5, 1);
+   digitalWrite(8, 0);
+    }
+    if (val == 3) {
+      motor1.run(BACKWARD);
+      motor2.run(FORWARD);
+      motor1.setSpeed(250);
+      motor2.setSpeed(250);
+      digitalWrite(12, 0);
+      digitalWrite(13 , 0);
+        digitalWrite(7, 0);
+  digitalWrite(5, 1);
+   digitalWrite(8, 0);
+    }
+    if (val == 4) {
+      motor1.run(FORWARD);
+      motor2.run(FORWARD);
+      motor1.setSpeed(255);
+      motor2.setSpeed(255);
+      digitalWrite(12, 0);
+      digitalWrite(13 , 0);
+        digitalWrite(7, 0);
+  digitalWrite(5 , 1);
+   digitalWrite(8, 0);
+    }
+    if (val == 5)
+    {
+      if ( y1 <= 175 )
+        y1 += 1;
+      servl.write(y1);
+    }
+    if (val == 6)
+    {
+      if ( y1 >= 5 )
+        y1 -= 1;
+      servl.write(y1);
+    }
+    if (val == 7)
+    {
+      if ( y2 <= 175 )
+        y2 += 1;
+      servp.write(y2);
+    }
+    if (val == 8)
+    {
+      if ( y2 >= 5 )
+        y2 -= 1;
+      servp.write(y2);
+    }
+    if (val == 9)
+    {
+      if ( last ) {
+        digitalWrite(12, 0);
+        digitalWrite(13 , 0);
+          digitalWrite(7, 0);
+  digitalWrite(5 , 0);
+    digitalWrite(8,0);
+        last = 0;
+      }
+      else
+      { digitalWrite(8,0);
+      digitalWrite(12, 1);
+      delay(30);
+       digitalWrite(7,1);
+       delay(30);
+        digitalWrite(13 , 1);
+        delay(300);
+        digitalWrite(12 , 0);
+             digitalWrite(7,0);
+             digitalWrite(13,0);
+          digitalWrite(8, 1);
+          delay(3000);
+           digitalWrite(5,0);
+           digitalWrite(8,0);
   
-  
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-   }
-   if (val == 3){
-  motor1.run(BACKWARD);  
-  motor2.run(FORWARD);  
-  motor1.setSpeed(200);   
-  motor2.setSpeed(200);
-   }
-   if (val == 4){
-  motor1.run(FORWARD);  
-  motor2.run(FORWARD);  
-  motor1.setSpeed(255);   
-  motor2.setSpeed(255);
-  
-   } if (val == 5) {
-     for(int i = 0; i <= 180; i++)        
-  {
-    serv.write(i);
-  }                        
-  }
-  if (val == 6){
-    for (int i = 0; i>= -180; i--){
-      serv.write(i);
+        last = 1;
+      }
     }
   }
-  if (val == 7){
-      pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
-   
-  digitalWrite(12,HIGH);
-  digitalWrite(13,HIGH);
-  }
-  if (val == 8){
-      pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
-  digitalWrite(12,LOW);
-  digitalWrite(13 ,LOW);
-  }
-  
-  }
-  }
+}
